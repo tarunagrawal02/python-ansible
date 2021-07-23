296ddd5b6190d819931dfbac017e6ebfab061a0c
tarunagrawal02 <87853066+tarunagrawal02@users.noreply.github.com>
2021-07-23 14:12:02 +0530

    Add files via upload

---
+++ python/serviceMonitoring.py
@@ -0,0 +1,68 @@
+import psutil
+import socket
+import json
+import requests
+from datetime import datetime
+from flask import Flask
+from flask import request
+
+date = datetime.now()
+dateStr = date.strftime('%d.%m.%Y %H:%M:%S.%f')
+dt_obj = datetime.strptime(dateStr, '%d.%m.%Y %H:%M:%S.%f')
+timestamp = dt_obj.timestamp() * 1000
+
+#Check that the service is running
+
+host_name = socket.gethostname()
+services = ["RABBITMQ","HTTPD","POSTGRESQL"]
+detected_pids = set()
+rbcapp1 = "DOWN"
+service_status = "DOWN"
+serviceDict = {}
+serviceList = []
+# Search for running processes that match our command string.
+for proc in psutil.process_iter():
+  for service in services:
+    try:
+        if service in proc.name().upper():
+            detected_pids.add(str(proc.name))
+            service_status = "UP"
+            serviceDict = {
+                           "service_name": service,
+                           "service_status": service_status,
+                           "host_name": host_name
+                          }
+            serviceList.append(serviceDict)
+    # We could also get psutil.ZombieProcess or
+    # psutil.AccessDenied.
+
+    except psutil.NoSuchProcess:
+        pass
+
+#Creating the JSON file name format as required
+    fileName = service + "-status-" + str(int(timestamp)) + ".json"
+    with open(fileName, "w") as outfile:
+        json.dump(serviceDict, outfile)
+
+app = Flask(__name__)
+@app.route('/add',methods=['GET'])
+def addJson():
+   resp = requests.post('http://elasticsearch.com/add/', json=serviceDict)
+   if resp.status_code != 201:
+      raise ApiError('POST /add/ {}'.format(resp.status_code))
+   return resp.status_code
+
+@app.route('/healthcheck',methods=['GET'])
+def getAllHealthCheck():
+   if detected_pids.len() == services.len():
+      rbcapp1 = "UP"
+   return "The rbcapp1 application is" + rbcapp1
+
+@app.route('/healthcheck/<serviceName>',methods=['GET'])
+def getServiceHealthCheck(serviceName):
+   for ser in serviceList:
+      if ser['service_name'] == serviceName:
+         return ser['service_status']
+
+if __name__ == '__main__':
+   app.run()
